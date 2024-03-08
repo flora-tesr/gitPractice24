18f7241ded6fa3730c12100980c4487f58c2d543
Wumiti Wulubieke <wumiti.wulu@mail.com>
2024-03-07 21:55:52 -0500

    java file added

---
+++ src/test/java/ulugh.java
@@ -0,0 +1,25 @@
+public class ulugh {
+
+
+        private int[] array = new int[] {1,2,3};
+
+        public void testFor() {
+            for (int i : array) {
+                System.out.println(i);
+            }
+        }
+
+        public void testForIndex() {
+            for (int i=0;i<array.length;i++) {
+                System.out.println(array[i]);
+            }
+        }
+
+        public void testForIndex01() {
+            int len = array.length;
+            for (int i=0;i<len;i++) {
+                System.out.println(array[i]);
+            }
+        }
+
+    }
